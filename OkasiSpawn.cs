e162a3a36c6ab8d6d60f5b537190475d7fc86d2f
mekatama <tamatte@hotmail.com>
2018-06-19 09:37:29 +0900

    okasi生成スクリプト
    一定時間ごとに生成
    後でokasi種類増やす

---
+++ OkasiSpawn.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OkasiSpawn : MonoBehaviour {
+//	GameObject gameController;			//検索したオブジェクト入れる用
+	public GameObject[] okasiObject;	//okasiのプレハブを配列で管理
+	public float timeOut;				//okasiを出現させたい時間間隔
+	private float timeElapsed;			//時間を仮に格納する変数
+	private int okasiType;				//okasiの種類
+
+	void Start () {
+//		gameController = GameObject.FindWithTag ("GameController");	//GameControllerオブジェクトを探す
+	}
+
+	void Update () {
+		//時間チェック
+		timeElapsed += Time.deltaTime;	//経過時間の保存
+        if(timeElapsed >= timeOut) {	//指定した経過時間に達したら
+			OkasiGo();
+		}
+	}
+	
+	public void OkasiGo(){
+		okasiType = 0;							//(仮)okasiの種類
+		float x_pos = Random.Range(-3.5f,3.5f); //ランダムで出現位置を決める
+		//gcって仮の変数にGameControllerのコンポーネントを入れる
+//		GameController gc = gameController.GetComponent<GameController>();
+
+		//okasiを生成する
+		GameObject okasi = (GameObject)Instantiate(
+			okasiObject[okasiType],						//■仮で0を入れている。0～4を想定
+			new Vector3(x_pos, transform.position.y, transform.position.z),
+			transform.rotation
+		);
+		timeElapsed = 0.0f;			//生成時間リセット
+	}
+}
