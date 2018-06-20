1f3a286f7cf9ef47c14e899ba95e2468b4cc1e7b
mekatama <tamatte@hotmail.com>
2018-06-20 09:06:58 +0900

    UIタイムカウント表示

---
+++ UI_Time.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Time : MonoBehaviour {
+	public GameObject gameController;	//GameController取得
+	public Text timeText;				//Textコンポーネント取得用
+
+	void Update () {
+		//gcって仮の変数にGameControllerのコンポーネントを入れる
+		GameController gc = gameController.GetComponent<GameController>();
+		//time表示
+		timeText.text = gc.timeCount.ToString("000.000");
+	}
+}
