37a57129a6558a5330c09135997b243e0e3cef29
mekatama <tamatte@hotmail.com>
2018-06-20 09:06:39 +0900

    UIスコア表示

---
+++ UI_Score.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Score : MonoBehaviour {
+	public GameObject gameController;	//GameController取得
+	public Text scoreText;				//Textコンポーネント取得用
+	
+	void Update () {
+		//gcって仮の変数にGameControllerのコンポーネントを入れる
+		GameController gc = gameController.GetComponent<GameController>();
+		//score表示
+		scoreText.text = gc.totalScore.ToString("000000");
+	}
+}
