0e079b25828b88c186960d0c1c9b88bdf8e100d2
mekatama <tamatte@hotmail.com>
2018-06-22 09:03:06 +0900

    ボタン移動処理を入れる

--- Player.cs
+++ Player.cs
@@ -6,8 +6,8 @@ public class Player : MonoBehaviour {
 	CharacterController characterController;//コンポーネントを入れる用
 //	Animator animator;
 	public float graviy;					//重力値
-	private Vector3 velocity;				//最終的な速度
-	private float moveX = 0f;				//x方向移動用
+	public Vector3 velocity;				//最終的な速度
+	public float moveX = 0f;				//x方向移動用
 	public float speedX = 1.5f;				//x方向速度用
 
 	void Start () {
@@ -19,24 +19,28 @@ public class Player : MonoBehaviour {
 	void Update () {
 		//地上にいる時の処理
 		if(characterController.isGrounded){
+			//【ボタン移動したい時は、コメントアウトする】
 			 moveX = Input.GetAxis ("Horizontal") * speedX;	//左右入力でx方向のベクトル出す
 			 velocity.x = moveX;							//最終的な速度ベクトルに代入
-/*
-			//デバッグ用
-			if(Input.GetKeyDown("left")){
-				Debug.Log("go left");
-			}
-			if(Input.GetKeyDown("right")){
-				Debug.Log("go right");
-			}
-*/
 		}
 		//地上にいない時
 		else{
 			velocity.y -= graviy * Time.deltaTime;	//重力の加算
 		}
 
-		//移動実行
+//		Debug.Log(velocity.x);
 		characterController.Move(velocity * Time.deltaTime);
+	}
+
+	public void moveLeft(){
+		velocity.x = speedX * -1;							//最終的な速度ベクトルに代入
+	}
+
+	public void moveRight(){
+		velocity.x = speedX;							//最終的な速度ベクトルに代入
+	}
+
+	public void moveStop(){
+		velocity = Vector3.zero;							//最終的な速度ベクトルに代入
 	}
 }
