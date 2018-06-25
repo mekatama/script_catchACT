cf1720a740f4b5d5aee2de159b89afadbd000d44
mekatama <tamatte@hotmail.com>
2018-06-25 19:53:24 +0900

    クリアで操作不可に

--- Player.cs
+++ Player.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
+	GameObject gameController;			//検索したオブジェクト入れる用
 	CharacterController characterController;//コンポーネントを入れる用
 //	Animator animator;
 	public float graviy;					//重力値
@@ -11,12 +12,15 @@ public class Player : MonoBehaviour {
 	public float speedX = 1.5f;				//x方向速度用
 
 	void Start () {
+		gameController = GameObject.FindWithTag ("GameController");	//GameControllerオブジェクトを探す
 		characterController = GetComponent<CharacterController>();	//コンポーネントを取得
 //		animator = GetComponent<Animator>();	//コンポーネントを取得
 		velocity = Vector3.zero;				//0ベクトル
 	}
 	
 	void Update () {
+		//gcって仮の変数にGameControllerのコンポーネントを入れる
+		GameController gc = gameController.GetComponent<GameController>();
 		//地上にいる時の処理
 		if(characterController.isGrounded){
 			//【ボタン移動したい時は、コメントアウトする】
@@ -28,8 +32,10 @@ public class Player : MonoBehaviour {
 			velocity.y -= graviy * Time.deltaTime;	//重力の加算
 		}
 
-//		Debug.Log(velocity.x);
-		characterController.Move(velocity * Time.deltaTime);
+		if(!gc.isClear){
+//			Debug.Log(velocity.x);
+			characterController.Move(velocity * Time.deltaTime);
+		}
 	}
 
 	public void moveLeft(){
