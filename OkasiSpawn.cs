adec932d86cfb811b67cfc7fd611bf0ceed2822d
mekatama <tamatte@hotmail.com>
2018-06-25 19:52:44 +0900

    クリアでお菓子を出さない

--- OkasiSpawn.cs
+++ OkasiSpawn.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class OkasiSpawn : MonoBehaviour {
-//	GameObject gameController;			//検索したオブジェクト入れる用
+	GameObject gameController;			//検索したオブジェクト入れる用
 	public GameObject[] okasiObject;	//okasiのプレハブを配列で管理
 	public float timeOut;				//okasiを出現させたい時間間隔
 	private float timeElapsed;			//時間を仮に格納する変数
 	private int okasiType;				//okasiの種類
 
 	void Start () {
-//		gameController = GameObject.FindWithTag ("GameController");	//GameControllerオブジェクトを探す
+		gameController = GameObject.FindWithTag ("GameController");	//GameControllerオブジェクトを探す
 		okasiType = 0;							//(仮)okasiの種類
 	}
 
@@ -27,14 +27,16 @@ public class OkasiSpawn : MonoBehaviour {
 		okasiType = Random.Range(0, 2);			//(仮)okasiの種類。最終的にgamecontrollで制御
 		float x_pos = Random.Range(-3.5f,3.5f); //ランダムで出現位置を決める
 		//gcって仮の変数にGameControllerのコンポーネントを入れる
-//		GameController gc = gameController.GetComponent<GameController>();
+		GameController gc = gameController.GetComponent<GameController>();
 
-		//okasiを生成する
-		GameObject okasi = (GameObject)Instantiate(
-			okasiObject[okasiType],						//■仮で0を入れている。0～4を想定
-			new Vector3(x_pos, transform.position.y, transform.position.z),
-			transform.rotation
-		);
+		if(!gc.isClear){
+			//okasiを生成する
+			GameObject okasi = (GameObject)Instantiate(
+				okasiObject[okasiType],						//■仮で0を入れている。0～4を想定
+				new Vector3(x_pos, transform.position.y, transform.position.z),
+				transform.rotation
+			);
+		}
 		timeElapsed = 0.0f;			//生成時間リセット
 	}
 }
