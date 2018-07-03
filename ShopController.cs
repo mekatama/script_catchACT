f7e28e13e3807430a83af4b97df3202cd050e72b
mekatama <tamatte@hotmail.com>
2018-07-03 19:32:40 +0900

    shop画面制御用

---
+++ ShopController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ShopController : MonoBehaviour {
+//	public int Item1_Point = 10;	//Item1の価格
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	//戻るボタン用の制御関数
+	public void ButtonClicked_Title(){
+		SceneManager.LoadScene("Title");	//シーンのロード
+	}
+}
