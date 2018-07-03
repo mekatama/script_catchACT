1c324464931a0bfb555a6d3c6d3c0f4f4b5d9ce6
mekatama <tamatte@hotmail.com>
2018-07-03 19:34:30 +0900

    shop画面お菓子ポイント表示用

---
+++ UIshop_OkasiPoint.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIshop_OkasiPoint : MonoBehaviour {
+	public Text okasiPointText;			//Textコンポーネント取得用
+	
+	void Update () {
+		//ショップ用okasipoint表示
+		okasiPointText.text = PlayerPrefs.GetInt("totalOkasi").ToString("000000");
+	}
+}
