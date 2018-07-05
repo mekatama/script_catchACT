3e7a79dcd8c4fac3d0216a5a091ba53355aa670b
mekatama <tamatte@hotmail.com>
2018-07-05 19:33:24 +0900

    itemの名前と価格を設定、表示

--- UIshop_Item.cs
+++ UIshop_Item.cs
@@ -4,58 +4,63 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class UIshop_Item : MonoBehaviour {
-//	public GameObject shopController;	//ShopController取得
 	public int tempSave;			//
-	public int itemPoint1 = 0;		//Itemの価格
-	public int itemPoint2 = 0;		//Itemの価格
-	public int itemPoint3 = 0;		//Itemの価格
-	public int itemPoint4 = 0;		//Itemの価格
-	public int itemPoint5 = 0;		//Itemの価格
-	public int itemPoint6 = 0;		//Itemの価格
+	public string[] itemName;		//Itemの名前(配列)
+	public Text[] itemNameText;		//Textコンポーネント取得用
+	public int[] itemPoint;			//Itemの価格
+	public Text[] itemPointText;	//Textコンポーネント取得用
 
 	void Start () {
+	}
+
+	void Update () {
+		//値段表示
+		for(int i = 0; i < itemName.Length; i++) {
+			itemNameText[i].text = itemName[i];
+			itemPointText[i].text = itemPoint[i].ToString("000");
+		}
 	}
 
 	//shop item用のbutton制御関数
 	public void ButtonClicked_Item1(){
 		tempSave = PlayerPrefs.GetInt("totalOkasi");
-		tempSave = tempSave - itemPoint1;
+		tempSave = tempSave - itemPoint[0];
 		PlayerPrefs.SetInt("totalOkasi", tempSave);	//save
-		Debug.Log("item 1 buy : " + itemPoint1);
+		Debug.Log("item 1 buy : " + itemPoint[0]);
 	}
 	//shop item用のbutton制御関数
 	public void ButtonClicked_Item2(){
 		tempSave = PlayerPrefs.GetInt("totalOkasi");
-		tempSave = tempSave - itemPoint2;
+		tempSave = tempSave - itemPoint[1];
 		PlayerPrefs.SetInt("totalOkasi", tempSave);	//save
-		Debug.Log("item 2 buy : " + itemPoint2);
+		Debug.Log("item 2 buy : " + itemPoint[1]);
 	}
 	//shop item用のbutton制御関数
 	public void ButtonClicked_Item3(){
 		tempSave = PlayerPrefs.GetInt("totalOkasi");
-		tempSave = tempSave - itemPoint3;
+		tempSave = tempSave - itemPoint[2];
 		PlayerPrefs.SetInt("totalOkasi", tempSave);	//save
-		Debug.Log("item 3 buy : " + itemPoint3);
+		Debug.Log("item 3 buy : " + itemPoint[2]);
 	}
 	//shop item用のbutton制御関数
 	public void ButtonClicked_Item4(){
 		tempSave = PlayerPrefs.GetInt("totalOkasi");
-		tempSave = tempSave - itemPoint4;
+		tempSave = tempSave - itemPoint[3];
 		PlayerPrefs.SetInt("totalOkasi", tempSave);	//save
-		Debug.Log("item 4 buy : " + itemPoint4);
+		Debug.Log("item 4 buy : " + itemPoint[3]);
 	}
 	//shop item用のbutton制御関数
 	public void ButtonClicked_Item5(){
 		tempSave = PlayerPrefs.GetInt("totalOkasi");
-		tempSave = tempSave - itemPoint5;
+		tempSave = tempSave - itemPoint[4];
 		PlayerPrefs.SetInt("totalOkasi", tempSave);	//save
-		Debug.Log("item 5 buy : " + itemPoint5);
+		Debug.Log("item 5 buy : " + itemPoint[4]);
 	}
 	//shop item用のbutton制御関数
 	public void ButtonClicked_Item6(){
 		tempSave = PlayerPrefs.GetInt("totalOkasi");
-		tempSave = tempSave - itemPoint6;
+		tempSave = tempSave - itemPoint[5];
 		PlayerPrefs.SetInt("totalOkasi", tempSave);	//save
-		Debug.Log("item 6 buy : " + itemPoint6);
+		Debug.Log("item 6 buy : " + itemPoint[5]);
 	}
 }
