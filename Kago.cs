d3c8df9ae890db90e860a1a6a8b4c5bfa4118d08
mekatama <tamatte@hotmail.com>
2018-07-10 19:15:24 +0900

    itemで横幅を拡げる処理

---
+++ Kago.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kago : MonoBehaviour {
+	GameObject gameController;			//検索したオブジェクト入れる用
+
+	void Start () {
+		gameController = GameObject.FindWithTag ("GameController");	//GameControllerオブジェクトを探す
+		//gcって仮の変数にGameControllerのコンポーネントを入れる
+		GameController gc = gameController.GetComponent<GameController>();
+		//kagoの大きさ設定
+		this.transform.localScale = new Vector3(gc.playerKagoScale, 0.1f, 1);
+	}
+	
+	void Update () {
+		
+	}
+}
