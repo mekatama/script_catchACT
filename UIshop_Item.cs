c6f9ae1ad7c03700f2d09af5dac556c2e638e2f6
mekatama <tamatte@hotmail.com>
2018-07-10 19:16:26 +0900

    籠大きくアイテム処理

--- UIshop_Item.cs
+++ UIshop_Item.cs
@@ -9,12 +9,14 @@ public class UIshop_Item : MonoBehaviour {
 	public Text[] itemNameText;		//Textコンポーネント取得用
 	public int[] itemPoint;			//Itemの価格
 	public Text[] itemPointText;	//Textコンポーネント取得用
-	private float savePlayerSpeedItem;	//
-	public float itemSpeed;
+	private float savePlayerSpeedItem;	//saveデータ一時保存用
+	private int saveKagoScaleItemNum;	//saveデータ一時保存用
+	public float itemSpeed;			//アイテムのspeedUp値
 
 	void Start () {
 		//saveがなかったら０を入れて初期化
-		savePlayerSpeedItem = PlayerPrefs.GetFloat("playerSpeedItem", 0); 
+		savePlayerSpeedItem = PlayerPrefs.GetFloat("playerSpeedItem", 0); 	
+		saveKagoScaleItemNum = PlayerPrefs.GetInt("kagoScaleItemNum", 0); 
 	}
 
 	void Update () {
@@ -25,7 +27,7 @@ public class UIshop_Item : MonoBehaviour {
 		}
 	}
 
-	//shop item用のbutton制御関数
+	//speedUp item用のbutton制御関数
 	public void ButtonClicked_Item1(){
 		tempSave = PlayerPrefs.GetInt("totalOkasi");
 		tempSave = tempSave - itemPoint[0];
@@ -36,12 +38,16 @@ public class UIshop_Item : MonoBehaviour {
 		PlayerPrefs.SetFloat("playerSpeedItem", savePlayerSpeedItem);	//save
 		Debug.Log("speed item : " + PlayerPrefs.GetFloat("playerSpeedItem"));
 	}
-	//shop item用のbutton制御関数
+	//籠大きくする item用のbutton制御関数
 	public void ButtonClicked_Item2(){
 		tempSave = PlayerPrefs.GetInt("totalOkasi");
 		tempSave = tempSave - itemPoint[1];
 		PlayerPrefs.SetInt("totalOkasi", tempSave);	//save
 		Debug.Log("item 2 buy : " + itemPoint[1]);
+		//強化内容
+		saveKagoScaleItemNum = saveKagoScaleItemNum + 1;				//加算
+		PlayerPrefs.SetInt("kagoScaleItemNum", saveKagoScaleItemNum);	//save
+		Debug.Log("scale item : " + PlayerPrefs.GetInt("kagoScaleItemNum"));
 	}
 	//shop item用のbutton制御関数
 	public void ButtonClicked_Item3(){
