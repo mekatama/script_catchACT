e3f589f250bf8b45085690796daeafc278c865d4
mekatama <tamatte@hotmail.com>
2018-07-24 19:29:06 +0900

    削除予定

---
+++ UIshop_ItemPoint.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIshop_ItemPoint : MonoBehaviour {
+//.	public GameObject shopController;	//ShopController取得
+	public Text itemPointText;			//Textコンポーネント取得用
+
+	void Start () {
+//		shopController = GameObject.FindWithTag ("UIController");	//ShopControllerオブジェクトを探す
+	}
+	
+	void Update () {
+		//scって仮の変数にShopControllerのコンポーネントを入れる
+//		ShopController sc = shopController.GetComponent<ShopController>();
+		//値段表示
+//		itemPointText.text = sc.Item1_Point.ToString("000");
+	}
+}
