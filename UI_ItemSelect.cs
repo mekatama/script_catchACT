ae53e793f7b2f7adc62c970c53fc8917f1eabbdf
mekatama <tamatte@hotmail.com>
2018-07-24 19:27:16 +0900

    point upアイテムのセレクト画面でのテキスト表示

--- UI_ItemSelect.cs
+++ UI_ItemSelect.cs
@@ -11,22 +11,26 @@ public class UI_ItemSelect : MonoBehaviour {
 //	private float savePlayerSpeedItem;	//saveデータ一時保存用
 	private int savePlayerSpeedItemNum;	//saveデータ一時保存用
 	private int saveKagoScaleItemNum;	//saveデータ一時保存用
+	private int savePointUpItemNum;		//saveデータ一時保存用
 
 	void Start () {
 		//saveがなかったら０を入れて初期化
 //		savePlayerSpeedItem = PlayerPrefs.GetFloat("playerSpeedItem", 0); 	
 		savePlayerSpeedItemNum = PlayerPrefs.GetInt("playerSpeedItemNum", 0); 	
 		saveKagoScaleItemNum = PlayerPrefs.GetInt("kagoScaleItemNum", 0); 
+		savePointUpItemNum = PlayerPrefs.GetInt("pointUpItemNum", 0); 
 	}
 	
 	void Update () {
 		savePlayerSpeedItemNum = PlayerPrefs.GetInt("playerSpeedItemNum", 0); 	
 		saveKagoScaleItemNum = PlayerPrefs.GetInt("kagoScaleItemNum", 0); 
+		savePointUpItemNum = PlayerPrefs.GetInt("pointUpItemNum", 0); 
 		//アイテム名と個数の表示
 		for(int i = 0; i < itemName.Length; i++) {
 			itemNameText[i].text = itemName[i];
 			itemNumText[0].text = savePlayerSpeedItemNum.ToString("000");
 			itemNumText[1].text = saveKagoScaleItemNum.ToString("000");
+			itemNumText[2].text = savePointUpItemNum.ToString("000");
 //			itemNumText[i].text = itemNum[i].ToString("000");
 		}
 	}
